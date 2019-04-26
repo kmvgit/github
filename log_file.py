eefbe69e6f4724255f9012c50da2882c29dc7783
Mikhail <kmvmail@mail.ru>
2019-04-26 16:17:31 +0300

    test

--- log_file.py
+++ log_file.py
@@ -3,11 +3,32 @@ import re
 client = dict()
 p = re.compile('^(\d{1,3}.?){4}')
 for line in open('access2.log', 'rt'):
-    ip_client = p.match(line).group(0)
-    if ip_client in client:
-        client[ip_client] += 1
+    try:
+        ip_client = p.match(line).group(0)
+    except:
+        continue
     else:
-        client.update({ip_client: 1})
+        if ip_client in client:
+            client[ip_client] += 1
+        else:
+            client.update({ip_client: 1})
 
 print([id for id in sorted(client.items(), key = lambda item: item[1],
                               reverse = True)][:10])
+
+
+platform = dict()
+p = re.compile('^(?:\d{1,3}.?){4}[\s\S]*? \d{3} [^"]*?"[^"]*?" "[^\(]*?\((['
+               '^\)]*?)\)')
+for line in open('access2.log', 'rt'):
+    try:
+        platform_name = p.match(line).group(1)
+    except:
+        continue
+    else:
+        if platform_name in platform:
+            platform[platform_name] += 1
+        else:
+            platform.update({platform_name: 1})
+print([id for id in sorted(platform.items(), key = lambda item: item[1],
+                              reverse = True)][:5])
