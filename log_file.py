a43ba270d8b33540c1afde66a4edad787c0f1155
Mikhail <kmvmail@mail.ru>
2019-04-29 16:32:19 +0300

    test

--- log_file.py
+++ log_file.py
@@ -40,3 +40,29 @@ reg_platform = re.compile('^(?:\d{1,3}.?){4}[\s\S]*? \d{3} [^"]*?"[^"]*?" "[^\(]
                                   '^\)]*?)\)')
 for line_platform in popular_platform(reg_platform, log, 5):
     print(line_platform)
+
+
+# Вариант с одной функцией
+def popular(reg_str, log, count_str):
+    dict_str = coll.defaultdict(int)
+    with open(log, 'rt') as file_log:
+        for line in file_log:
+            try:
+                str_data = reg_str.match(line).group(1)
+            except:
+                continue
+            dict_str[str_data] += 1
+    list_data = coll.Counter(dict_str).most_common(count_str)
+    return list_data
+
+
+print('_________________')
+reg_customer = re.compile('(^(\d{1,3}.?){4})')
+for line in popular(reg_customer, log, 10):
+    print(line)
+
+print('_________________')
+reg_platform = re.compile('^(?:\d{1,3}.?){4}[\s\S]*? \d{3} [^"]*?"[^"]*?" "['
+                          '^\(]*?\((([^\)](?!bot))*?)\)')
+for line in popular(reg_platform, log, 5):
+    print(line)
