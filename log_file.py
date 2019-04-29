297d98e75bf237051c8b259311ca329c22848260
Mikhail <kmvmail@mail.ru>
2019-04-29 15:35:12 +0300

    test

--- log_file.py
+++ log_file.py
@@ -2,13 +2,12 @@ import re
 import collections as coll
 
 # 10 клиентов посетившие большее количество страниц
-def popular_customer(log, count_str):
+def popular_customer(reg_str, log, count_str):
     dict_str = coll.defaultdict(int)
     with open(log, 'rt') as file_log:
-        reg_customer = re.compile('^(\d{1,3}.?){4}')
         for line in file_log:
             try:
-                customer_ip = reg_customer.match(line).group(0)
+                customer_ip = reg_str.match(line).group(0)
             except:
                 continue
             dict_str[customer_ip] += 1
@@ -16,14 +15,12 @@ def popular_customer(log, count_str):
     return list_ip
 
 # 5 самых популярных платформ
-def popular_platform(log, count_str):
+def popular_platform(reg_str, log, count_str):
     dict_str = coll.defaultdict(int)
     with open(log, 'rt') as file_log:
-        reg_platform = re.compile('^(?:\d{1,3}.?){4}[\s\S]*? \d{3} [^"]*?"[^"]*?" "[^\(]*?\((['
-                                  '^\)]*?)\)')
         for line in file_log:
             try:
-                platform_name = reg_platform.match(line).group(1)
+                platform_name = reg_str.match(line).group(1)
             except:
                 continue
             if 'bot' not in platform_name.lower():
@@ -34,9 +31,12 @@ def popular_platform(log, count_str):
 log = 'access2.log'
 
 print("_________________")
-for line_ip in popular_customer(log, 10):
+reg_customer = re.compile('^(\d{1,3}.?){4}')
+for line_ip in popular_customer(reg_customer, log, 10):
     print(line_ip)
 
 print('_________________')
-for line_platform in popular_platform(log, 5):
+reg_platform = re.compile('^(?:\d{1,3}.?){4}[\s\S]*? \d{3} [^"]*?"[^"]*?" "[^\(]*?\((['
+                                  '^\)]*?)\)')
+for line_platform in popular_platform(reg_platform, log, 5):
     print(line_platform)
