753501d9527461dc66319a621876b55cab776245
Mikhail <kmvmail@mail.ru>
2019-05-14 12:24:28 +0300

    test

--- test_task.py
+++ test_task.py
@@ -98,7 +98,10 @@ def duration_flight(start, finish):
 
     start = start.split(':')
     finish = finish.split(':')
-    delta_hour = int(finish[0]) + 24 - int(start[0])
+    if int(finish[0]) < int(start[0]):
+        delta_hour = int(finish[0]) + 24 - int(start[0])
+    else:
+        delta_hour = int(finish[0]) - int(start[0])
     delta_minute = int(finish[1]) - int(start[1])
     if delta_minute < 0:
         delta_hour -= 1
