7dce94a72bef12bef234361c675805e81b709b5c
Mikhail <kmvmail@mail.ru>
2019-05-27 16:01:33 +0300

    test

---
+++ test.py
@@ -0,0 +1,10 @@
+import requests
+import os
+
+os.environ['HTTP_PROXY'] = 'http://localhost:8888'
+os.environ['HTTPS_PROXY'] = 'https://localhost:8888'
+os.environ['CURL_CA_BUNDLE'] = ''
+
+s = requests.session()
+g = s.get('http://yandex.ru?ggg=ggg').text
+print(g)
